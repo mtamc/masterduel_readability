ac2a40c88d2beb06da7ebb99e13e54fd6bd7de38
mtc <no@email.com>
2026-01-08 06:18:32 +0100

    v70: fix BOUNCE wording

--- src/Main.hs
+++ src/Main.hs
@@ -610,13 +610,17 @@ reword findAndReplaceP = fromRight (error "") . parse
   ""
 
 rewordBounce ∷ Text → Text
-rewordBounce = Text.replace "returned to the hand" "bounced" . Text.replace "returned to the hand/Extra Deck" "bounced" . reword do
+rewordBounce
+  = Text.replace "returned to the hand" "bounced"
+  . Text.replace "returned to the hand/Extra Deck" "bounced"
+  . Text.replace "returned to the hand or Extra Deck" "bounced"
+  . reword do
   string' "return "
   thingToBounce ← someTill anyNotConjunction $ try do
     string " "
     optional (string "from the field ")
     string "to the hand"
-    optional (string "/Extra Deck")
+    optional (string "/Extra Deck" <|> string " or Extra Deck")
   pure $ "BOUNCE " ⊕ thingToBounce
 
 -- Piercing wordings:
